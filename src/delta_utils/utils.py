307a24edc6bfffd432b27b6c1b3a4dff3f305fbd
Henry Pulver <henrypulver@hotmail.co.uk>
2022-11-04 14:51:05 +0000

    Jimmy comments + make pc command

--- src/delta_utils/utils.py
+++ src/delta_utils/utils.py
@@ -14,16 +14,21 @@ DONT_SEARCH = [
 
 
 def find_file(filename: str) -> Path:
-    """Find game_mechanics.py."""
+    """Find game_mechanics.py within all child directories of the directory this is run from."""
+    # Gets directory we're running from
     path = Path("").resolve()
 
+    # Checks current directory
     if (path / filename).exists():
         return path / filename
 
+    # Checks all child directories
     for item in path.iterdir():
+        # Skip files and directories we don't want to search
         if item.name in DONT_SEARCH or item.is_file():
             continue
 
+        # Search all ancestors
         if paths := list(filter(lambda x: x.name == filename, item.rglob("*"))):
             return paths[0]
     raise FileNotFoundError(f"Could not find {filename}")
